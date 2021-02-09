3c4a47c1fca598b4c656129e64eaf495f715fbda
Ishaquekha <78799676+Ishaquekha@users.noreply.github.com>
2021-02-09 17:50:58 +0500

    IR Sensor with coding in Arduino
    Simple Projects in IR Sensor

---
+++ sketch_feb03h.ino
@@ -0,0 +1,31 @@
+/*
+* IR sensor tester
+*/
+int IRSensor = 2; 
+int LED = 13; 
+
+
+
+void setup() 
+{
+
+
+
+  pinMode (IRSensor, INPUT); 
+  pinMode (LED, OUTPUT); 
+}
+
+void loop()
+{
+  int statusSensor = digitalRead (IRSensor);
+  
+  if (statusSensor == 1)
+    digitalWrite(LED, LOW); 
+  }
+  
+  else
+  {
+    digitalWrite(LED, HIGH); 
+  }
+  
+}
